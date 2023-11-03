854d1731e3da79d08bf3f56a6774bc916c840521
jadhavkirangreatlearning <jadhavkiran.greatlearning@gmail.com>
2023-11-03 10:00:43 +0530

    [STNO-1001] added hello api

--- springbootjavamailapiex/src/main/java/com/csi/controller/EmailController.java
+++ springbootjavamailapiex/src/main/java/com/csi/controller/EmailController.java
@@ -4,10 +4,7 @@ import com.csi.model.EmailModel;
 import com.csi.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 
@@ -22,5 +19,10 @@ public class EmailController {
     public ResponseEntity<String> sendEmail(@RequestBody EmailModel emailModel) throws MessagingException {
         emailService.sendEmail(emailModel);
         return ResponseEntity.ok("Email Sent Successfully");
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "WELCOME";
     }
 }
